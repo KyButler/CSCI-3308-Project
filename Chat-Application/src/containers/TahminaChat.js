82d3a6bac359a5f9ed82e4a146207217d9dcbad8
Tahmina <46764248+tahminaahmad@users.noreply.github.com>
2019-11-26 18:06:59 -0700

    Update TahminaChat.js
    changed class to className because I think it's supposed to be className in react...?

--- Chat-Application/src/containers/TahminaChat.js
+++ Chat-Application/src/containers/TahminaChat.js
@@ -6,57 +6,57 @@ import "./Chat.css";
 export default function Chat(props) {
   document.title = 'Chat';
   return (
-    <div class="container-fluid">
-    <div class="row">
-      <div class="col-2">
-        <div class="card">
-          <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#channels">Channels</button>
-          <div id="channels" class="collapse">
-            <ul id="channels-list" class="list-group">
-              <li class="list-group-item">John Doe</li>
-              <li class="list-group-item">Jane Doe</li>
-              <li class="list-group-item">Jimmy Doe</li>
+    <div className="container-fluid">
+    <div className="row">
+      <div className="col-2">
+        <div className="card">
+          <button type="button" className="btn btn-primary" data-toggle="collapse" data-target="#channels">Channels</button>
+          <div id="channels" className="collapse">
+            <ul id="channels-list" className="list-group">
+              <li className="list-group-item">John Doe</li>
+              <li className="list-group-item">Jane Doe</li>
+              <li className="list-group-item">Jimmy Doe</li>
             </ul>
           </div>
         </div>
-        <div class="card">
-        <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#groups">Groups</button>
-          <div id="groups" class="collapse">
-            <ul id="groups-list" class="list-group">
-              <li class="list-group-item">Software Development</li>
-              <li class="list-group-item">Business Leaders</li>
-              <li class="list-group-item">Research Project</li>
+        <div className="card">
+        <button type="button" className="btn btn-primary" data-toggle="collapse" data-target="#groups">Groups</button>
+          <div id="groups" className="collapse">
+            <ul id="groups-list" className="list-group">
+              <li className="list-group-item">Software Development</li>
+              <li className="list-group-item">Business Leaders</li>
+              <li className="list-group-item">Research Project</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="col-8">
-        <div class="card">
-          <div class="card-header">
+      <div className="col-8">
+        <div className="card">
+          <div className="card-header">
             <h5 id="header-name">John Doe</h5>
           </div>
-          <div id="messages" class="card-body">
-            <div class="overflow-auto">
-              <ul id="messages-list" class="list-group">
-                <li class="list-group-item">Hey!</li>
-                <li class="list-group-item">Hi!</li>
-                <li class="list-group-item">How are you?</li>
-                <li class="list-group-item">Great!</li>
+          <div id="messages" className="card-body">
+            <div className="overflow-auto">
+              <ul id="messages-list" className="list-group">
+                <li className="list-group-item">Hey!</li>
+                <li className="list-group-item">Hi!</li>
+                <li className="list-group-item">How are you?</li>
+                <li className="list-group-item">Great!</li>
               </ul>
             </div>
           </div>
-          <div class="card-footer">
+          <div className="card-footer">
             <input type="text"/>
           </div>
         </div>
       </div>
-      <div class="col-2">
-        <div class="card">
-          <ul id="online-list" class="list-group">
-            <li class="list-group-item">Carl Benny</li>
-            <li class="list-group-item">Benny Carl</li>
-            <li class="list-group-item">Joe Doe</li>
-            <li class="list-group-item">Doe John</li>
+      <div className="col-2">
+        <div className="card">
+          <ul id="online-list" className="list-group">
+            <li className="list-group-item">Carl Benny</li>
+            <li className="list-group-item">Benny Carl</li>
+            <li className="list-group-item">Joe Doe</li>
+            <li className="list-group-item">Doe John</li>
           </ul>
         </div>
       </div>
